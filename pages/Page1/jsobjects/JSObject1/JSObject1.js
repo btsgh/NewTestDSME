08530a0a6bb67598c067efebdcf8ac9ffb51d976
aparna123 <aparna123@appsmith.com>
2023-05-01 10:57:48 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+	},
+	myFun2: async () => {
+		//use async-await or promises
+		IconButton1.isVisible
+		ButtonGroup1.isVisible
+		Button3.isDisabled
+		Checkbox1.isChecked
+		CheckboxGroup1.isValid
+		MenuButton1.isVisible
+	}
+}
